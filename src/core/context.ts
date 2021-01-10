e28951dc59c8fb16563deebcce102891a973188b
ValeriaVG <valeria.viana.gusmao@gmail.com>
2021-01-10 16:34:52 +0100

    Make app crash on wrong redis credentials

--- src/core/context.ts
+++ src/core/context.ts
@@ -1,6 +1,16 @@
 import Redis from "ioredis";
-import { redis as redisOptions } from "../config";
+import { redis as redisUrl } from "../config";
 
-export const redis = new Redis(redisOptions);
+export const redis = new Redis(redisUrl, { lazyConnect: true });
+
+redis
+  .connect()
+  .then(() => {
+    console.log("✅ Redis connection: OK");
+  })
+  .catch((error) => {
+    console.error("🚨 Redis connection: ", error.message);
+    process.exit(1);
+  });
 
 export const log = console;
