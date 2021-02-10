4bedfbaa7d6dde58fab30c673dc416619a6315a5
ValeriaVG <valeria.viana.gusmao@gmail.com>
2021-02-10 18:13:58 +0100

    chore: rename AMP CMS

--- src/dashboard/pages/Home.ts
+++ src/dashboard/pages/Home.ts
@@ -2,6 +2,6 @@ import { html } from "amp/lib";
 import styles from "./styles.scss";
 
 export default html`<main class="${styles.main}">
-  <h1>AMP CMS</h1>
+  <h1>TWS CMS</h1>
   <p>Dashboard in development</p>
 </main>`;
